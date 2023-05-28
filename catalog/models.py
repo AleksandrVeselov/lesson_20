88a03a1d3b798479623ed7ddf92045501f85b30c
Aleksandr Veselov <sanek_ves@mail.ru>
2023-05-28 21:57:35 +0300

    Задания_2_3

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Название')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    image = models.ImageField(upload_to='products/', blank=True, null=True, verbose_name='Изображение')
+    category = models.ForeignKey('catalog.Category', on_delete=models.SET_NULL, null=True, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена')
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Название')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
