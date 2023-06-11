ca5f0a5311eea6723022391a5152c7006a9ca5d2
Aleksandr Veselov <sanek_ves@mail.ru>
2023-06-11 22:01:17 +0300

    CRUD для модели Blog реализован

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
-from catalog.views import ProductListView, ContactsListView, BlogList, BlogDetailView, BlogCreatePost
+from catalog.views import ProductListView, ContactsListView, BlogList, BlogDetailView, BlogCreatePost, BlogUpdatePost, \
+    BlogDeletePost
 
 urlpatterns = [
     path('', ProductListView.as_view()),
     path('contacts/', ContactsListView.as_view()),
     path('blog/', BlogList.as_view()),
-    path('blog/<slug>/', BlogDetailView.as_view()),
-    path('blog/create/', BlogCreatePost.as_view())
+    path('blog/<slug:slug>/', BlogDetailView.as_view()),
+    path('create_post/', BlogCreatePost.as_view()),
+    path('update_post/<slug:slug>/', BlogUpdatePost.as_view()),
+    path('delete_post/<slug:slug>/', BlogDeletePost.as_view())
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, TemplateView, DetailView, CreateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, TemplateView, DetailView, CreateView, UpdateView, DeleteView
 
 from catalog.models import Product, Blog
 
@@ -28,6 +29,10 @@ class BlogList(ListView):
     model = Blog
     extra_context = {'title': 'Блог'}
 
+    def get_queryset(self):
+        """Отбор постов у которых is_published=True"""
+        return Blog.objects.filter(is_published=True)
+
 
 class BlogDetailView(DetailView):
     model = Blog
@@ -35,4 +40,17 @@ class BlogDetailView(DetailView):
 
 class BlogCreatePost(CreateView):
     model = Blog
+    fields = ('title', 'slug', 'content', 'image', 'is_published')
+    success_url = '/blog/'
+
+
+class BlogUpdatePost(UpdateView):
+    model = Blog
+    fields = ('title', 'slug', 'content', 'image', 'is_published')
+    success_url = '/blog/'
+
+
+class BlogDeletePost(DeleteView):
+    model = Blog
+    success_url = '/blog/'
 
