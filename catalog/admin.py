b836ac6789075761459f6389a3c1e7a55f602958
Aleksandr Veselov <sanek_ves@mail.ru>
2023-06-12 17:57:25 +0300

    Написаны комментарии

--- catalog/admin.py
+++ catalog/admin.py
@@ -6,17 +6,23 @@ from catalog.models import Category, Product, Blog
 # Register your models here.
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['pk', 'title']
+    """Админ-панель для работы с моделью Category"""
+
+    list_display = ['pk', 'title']  # Поля для отображения в админ-панели
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['pk', 'title', 'price', 'category']
-    list_filter = ('category',)
-    search_fields = ('title', 'description')
+    """Админ-панель для работы с моделью Product"""
+
+    list_display = ['pk', 'title', 'price', 'category']  # Поля для отображения в админ-панели
+    list_filter = ('category',)  # Поля, по которым можно фильтровать записи
+    search_fields = ('title', 'description')  # Поля, по которым производится поиск
 
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'content']
-    prepopulated_fields = {'slug': ('title',)}
+    """Админ-панель для работы с моделью Blog"""
+
+    list_display = ['title', 'slug', 'content']  # Поля для отображения в админ-панели
+    prepopulated_fields = {'slug': ('title',)}  # Поле для автоматического заполнения исходя из поля title
--- catalog/models.py
+++ catalog/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 
 
-# Create your models here.
 class Product(models.Model):
+    """Модель продукты"""
+
     title = models.CharField(max_length=50, verbose_name='Название')
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
     image = models.ImageField(upload_to='products/', blank=True, null=True, verbose_name='Изображение')
     category = models.ForeignKey('catalog.Category', on_delete=models.SET_NULL, null=True, verbose_name='Категория')
     price = models.IntegerField(verbose_name='Цена')
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)  # Дата создания (генерируется автоматически)
+    modified_at = models.DateTimeField(auto_now=True)  # Дата изменения (генерируется автоматически)
 
     def __str__(self):
+        """Строковое представление"""
         return f'{self.title} {self.price}'
 
     class Meta:
