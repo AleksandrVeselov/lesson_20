c411ab095f1c186a148462f320cb9e3e859ec113
aleksandrveselov <122647162+AleksandrVeselov@users.noreply.github.com>
2023-06-14 21:39:09 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -134,7 +134,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 EMAIL_HOST = 'smtp.mail.ru'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = 'sanek_ves@mail.ru'
-EMAIL_HOST_PASSWORD = 'nfvHRi7ceDiLb9EjZDjE'  # Здесь должен быть пароль для внешнего приложения из аккаунта mail.ru
+EMAIL_HOST_PASSWORD = '################'  # Здесь должен быть пароль для внешнего приложения из аккаунта mail.ru
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 
